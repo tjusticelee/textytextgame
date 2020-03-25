0a52b4e797d7f081ae328b6ba1f9d0dfc62aa7ee
Justice Lee <txawjcojlee@gmail.com>
2020-03-25 13:08:21 -0400

    minor fix in __init__.py

--- yourproject/__init__.py
+++ yourproject/__init__.py
@@ -1,12 +1,9 @@
 import os
 
 from flask import Flask, render_template
-
-def create_app(test_config=None):
-    '''Create the app and configure it'''
-    app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
