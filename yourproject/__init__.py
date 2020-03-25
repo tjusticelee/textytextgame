45784d5ecb5ee9c3b09f75360beac4fdb98dd34c
Justice Lee <txawjcojlee@gmail.com>
2020-03-25 14:52:01 -0400

    working start screen

--- yourproject/__init__.py
+++ yourproject/__init__.py
@@ -1,8 +1,30 @@
-from . import index
-app.register_blueprint(index.bp)
-app.add_url_rule('/', endpoint='index')
+import os
 
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
